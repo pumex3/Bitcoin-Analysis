57191d4776569433c60f1cf9653a941bcecf00a1
pedro <pedro_aleixo22@hotmail.com>
2022-03-21 23:15:30 -0300

    AppBitcoin

---
+++ src/enums/Period.ts
@@ -0,0 +1,12 @@
+enum Period{
+    TEN_SECONDS = 10000,
+    THIRTY_SECONDS = 30000,
+    ONE_MINUTE = 60000,
+    FIVE_MINUTES = 300000,
+    TEN_MINUTES = 600000,
+    HALF_HOUR = 180000,
+    HOUR =  360000,
+    DAY = 86400000        
+}
+
+export default Period
